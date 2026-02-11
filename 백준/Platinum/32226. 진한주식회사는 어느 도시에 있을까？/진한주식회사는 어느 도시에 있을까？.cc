f8bced59e4be4dfb0dbf0ba5dfb9680cf1d0df75
chowin131 <chowin131@outlook.com>
2026-02-11 15:57:43 +0900

    [Platinum IV] Title: 진한주식회사는 어느 도시에 있을까?, Time: 0 ms, Memory: 2184 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/32226. 진한주식회사는 어느 도시에 있을까？/진한주식회사는 어느 도시에 있을까？.cc
+++ 백준/Platinum/32226. 진한주식회사는 어느 도시에 있을까？/진한주식회사는 어느 도시에 있을까？.cc
@@ -11,7 +11,11 @@ double cal_s(pt a,pt b,pt c){
 double angle(pt center,pt p1,pt p2){
     double ax=p1.x-center.x, ay=p1.y-center.y;
     double bx=p2.x-center.x, by=p2.y-center.y;
-    return acos((ax*bx+ay*by)/(sqrt(ax*ax+ay*ay)*sqrt(bx*bx+by*by)));
+    return acos(max(-1.0,min(1.0,(ax*bx+ay*by)/(sqrt(ax*ax+ay*ay)*sqrt(bx*bx+by*by)))));
+}
+double dist(pt a,pt b){
+    double dx=a.x-b.x,dy=a.y-b.y;
+    return sqrt(dx*dx+dy*dy);
 }
 signed main(){
     cout<<fixed;
@@ -24,6 +28,32 @@ signed main(){
     pt P1={(a*a+b*b-c*c)/(2*a),T/(2*a)};
     pt P2={(a*a-c*c)*(a*a+b*b-c*c)/(2*a*b*b),(c*c-a*a)*T/(2*a*b*b)};
     pt P3={(c*c*(a*a+b*b)-(a*a-b*b)*(a*a-b*b))/(2*a*c*c),(b*b-a*a)*T/(2*a*c*c)};
-    double area=cal_s(P1,P2,P3)+cal_a(c,angle(B,P1,P2))+cal_a(a,angle(C,P2,P3))+cal_a(b,angle(A,P3,P1));
-    cout<<area;
+    double tri=cal_s(P1,P2,P3);
+    if(tri>=0){
+        double area=tri+cal_a(c,angle(B,P1,P2))+cal_a(a,angle(C,P2,P3))+cal_a(b,angle(A,P3,P1));
+        cout<<area;
+    }
+    else{
+        pt cen[]={A,B,C};
+        double rad[]={b,c,a};
+        int pairs[][2]={{0,1},{1,2},{2,0}};
+        for(auto &p:pairs){
+            int i=p[0],j=p[1],k=3-i-j;
+            double d=dist(cen[i],cen[j]);
+            double ri=rad[i],rj=rad[j],rk=rad[k];
+            if(d>ri+rj||d<fabs(ri-rj)) continue;
+            double av=(ri*ri-rj*rj+d*d)/(2*d);
+            double h=sqrt(max(0.0,ri*ri-av*av));
+            double ex=(cen[j].x-cen[i].x)/d;
+            double ey=(cen[j].y-cen[i].y)/d;
+            pt q1={cen[i].x+av*ex+h*(-ey), cen[i].y+av*ey+h*ex};
+            pt q2={cen[i].x+av*ex-h*(-ey), cen[i].y+av*ey-h*ex};
+            if(dist(q1,cen[k])<=rk+1e-9 && dist(q2,cen[k])<=rk+1e-9){
+                double area=cal_a(ri,angle(cen[i],q1,q2))+cal_a(rj,angle(cen[j],q1,q2));
+                cout<<area;
+                return 0;
+            }
+        }
+        cout<<0;
+    }
 }
