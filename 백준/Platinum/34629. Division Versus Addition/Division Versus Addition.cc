37928c9bcde3be9ca8f9d1a63033f4fec30feccd
chowin131 <chowin131@outlook.com>
2026-02-27 02:16:37 +0900

    [Platinum III] Title: Division Versus Addition, Time: 108 ms, Memory: 4976 KB -BaekjoonHub

--- 백준/Platinum/34629. Division Versus Addition/Division Versus Addition.cc
+++ 백준/Platinum/34629. Division Versus Addition/Division Versus Addition.cc
@@ -1,27 +1,21 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-int main(){
-    ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
-    int t;
-    cin>>t;
-    while(t--){
-        int n,q;
+main(){
+    cin.tie(0)->sync_with_stdio(0);
+    int t,n,q,i,Log,r;
+    for(cin>>t;t--;){
         cin>>n>>q;
-        vector<int> v(n);
-        for(int&i:v)cin>>i,i--;
-        vector<int> need(n),line(n);
-        for(int i=0;i<n;i++){
-            int Log=log2(v[i]);
-            if(Log==0||Log!=log2(v[i])) Log++;
-            line[i]=(i>0?line[i-1]:0);
-            if((1<<Log)==v[i]) line[i]++;
-            need[i]=Log+(i>0?need[i-1]:0);
+        vector<int>v(n),N=v,L=v;
+        for(int&j:v)cin>>j,j--;
+        for(i=0;i<n;i++){
+            Log=log2(v[i]);
+            Log+=(Log==0||Log!=log2(v[i]));
+            L[i]=(1<<Log==v[i])+(i>0?L[i-1]:0);
+            N[i]=Log+(i>0?N[i-1]:0);
         }
-        for(int i=0;i<q;i++){
-            int l,r;
-            cin>>l>>r;
-            l--,r--;
-            cout<<need[r]-(l>0?need[l-1]:0)+(line[r]-(l>0?line[l-1]:0))/2<<"\n";
+        for(i=0;i<q;i++){
+            cin>>Log>>r;
+            cout<<N[r-1]-(Log>1?N[Log-2]:0)+(L[r-1]-(Log>1?L[Log-2]:0))/2<<"\n";
         }
     }
 }
