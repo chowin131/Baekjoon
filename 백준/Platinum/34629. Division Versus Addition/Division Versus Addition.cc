f896bada6ae896eb8aa26050020c55c889b6dd5e
chowin131 <chowin131@outlook.com>
2026-02-27 02:33:19 +0900

    [Platinum III] Title: Division Versus Addition, Time: 104 ms, Memory: 4976 KB -BaekjoonHub

--- 백준/Platinum/34629. Division Versus Addition/Division Versus Addition.cc
+++ 백준/Platinum/34629. Division Versus Addition/Division Versus Addition.cc
@@ -1,21 +1,2 @@
 #include<bits/stdc++.h>
-using namespace std;
-main(){
-    cin.tie(0)->sync_with_stdio(0);
-    int t,n,q,i,Log,r;
-    for(cin>>t;t--;){
-        cin>>n>>q;
-        vector<int>v(n),N=v,L=v;
-        for(int&j:v)cin>>j,j--;
-        for(i=0;i<n;i++){
-            Log=log2(v[i]);
-            Log+=(Log==0||Log!=log2(v[i]));
-            L[i]=(1<<Log==v[i])+(i>0?L[i-1]:0);
-            N[i]=Log+(i>0?N[i-1]:0);
-        }
-        for(i=0;i<q;i++){
-            cin>>Log>>r;
-            cout<<N[r-1]-(Log>1?N[Log-2]:0)+(L[r-1]-(Log>1?L[Log-2]:0))/2<<"\n";
-        }
-    }
-}
+using namespace std;main(){cin.tie(0)->sync_with_stdio(0);int t,n,q,i,G,r;for(cin>>t;t--;){cin>>n>>q;vector<int>v(n),N(n+1),L=N;for(int&j:v)cin>>j,j--;for(i=0;i<n;i++)G=log2(v[i]),G+=(G==0||G!=log2(v[i])),L[i+1]=(1<<G==v[i])+L[i],N[i+1]=G+N[i];while(q--)cin>>G>>r,cout<<N[r]-N[G-1]+(L[r]-L[G-1])/2<<"\n";}}
