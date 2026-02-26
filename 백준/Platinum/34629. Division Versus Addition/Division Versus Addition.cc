a5e3758e62c8a6216743a775bd600f0d356f6dc2
chowin131 <chowin131@outlook.com>
2026-02-27 03:03:47 +0900

    [Platinum III] Title: Division Versus Addition, Time: 104 ms, Memory: 3980 KB -BaekjoonHub

--- 백준/Platinum/34629. Division Versus Addition/Division Versus Addition.cc
+++ 백준/Platinum/34629. Division Versus Addition/Division Versus Addition.cc
@@ -1,2 +1,2 @@
 #include<bits/stdc++.h>
-using namespace std;main(){cin.tie(0)->sync_with_stdio(0);int t,n,q,i,G,r,v;for(cin>>t;t--;){cin>>n>>q;vector<int>N(n+1),L=N;for(i=0;i<n;i++)cin>>v,G=__lg(--v),G+=(G==0||G!=log2(v)),L[i+1]=(1<<G==v)+L[i],N[i+1]=G+N[i];while(q--)cin>>G>>r,cout<<N[r]-N[G-1]+(L[r]-L[G-1])/2<<"\n";}}
+using namespace std;main(){cin.tie(0)->sync_with_stdio(0);int t,n,q,i,v,l,r,x;for(cin>>t;t--;){cin>>n>>q;vector<int>N(n+1),L=N;for(i=1;i<=n;i++){for(cin>>v,x=1;x*2<=v;x*=2,N[i]++);N[i]+=N[i-1]+(x+1<v),L[i]=L[i-1]+(x+1==v);}while(q--)cin>>l>>r,cout<<N[r]-N[l-1]+(L[r]-L[l-1])/2<<"\n";}}
