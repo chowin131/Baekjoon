c2efbe0a89505b649c8c0d50e6f8519f254077ea
chowin131 <chowin131@outlook.com>
2026-03-04 01:03:45 +0900

    [Gold III] Title: 괄호 추가하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.cc
+++ 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.cc
@@ -1,53 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-int len;
-string str;
-vector<int> N;
-vector<char> O;
-int cal(int a, char op, int b){
-    if(op=='+') return a+b;
-    if(op=='-') return a-b;
-    return a*b;
+int n;
+string s;
+int cal(int x,char c,int y){
+	if(c=='+') return x+y;
+	if(c=='-') return x-y;
+	return x*y;
 }
-int solve(int mask){
-    int m=O.size();
-    int prev=-2;
-    for(int i=0;i<m;i++){
-        if(mask&(1<<i)){
-            if(i==prev+1) return INT32_MIN;
-            prev=i;
-        }
-    }
-    vector<int> v=N;
-    vector<char> o=O;
-    for(int i=m-1;i>=0;i--){
-        if(mask&(1<<i)){
-            int val=cal(v[i],o[i],v[i+1]);
-            v[i]=val;
-            v.erase(v.begin()+i+1);
-            o.erase(o.begin()+i);
-        }
-    }
-    int result=v[0];
-    for(int i=0;i<o.size();i++) result=cal(result,o[i],v[i+1]);
-    return result;
+int solve(int i,int cur){
+	int ans=INT32_MIN;
+	if(i>=n) return cur;
+	ans=max(ans,solve(i+2,cal(cur,s[i-1],s[i]-'0')));
+	if(i<n-2) ans=max(ans,solve(i+4,cal(cur,s[i-1],cal(s[i]-'0',s[i+1],s[i+2]-'0'))));
+	return ans;
 }
 int main(){
-    cin.tie(0)->sync_with_stdio(0);
-    cin>>len>>str;
-    for(int i=0;i<len;i++){
-        if(i%2==0) N.push_back(str[i]-'0');
-        else O.push_back(str[i]);
-    }
-    if(len==1){
-        cout<<str[0];
-        return 0;
-    }
-    int m=O.size();
-    int result=INT32_MIN;
-    for(int i=0;i<(1<<m);i++){
-        int val=solve(i);
-        if(val!=INT32_MIN) result=max(result,val);
-    }
-    cout<<result;
+	cin>>n>>s;
+	cout<<solve(2,s[0]-'0');
 }
