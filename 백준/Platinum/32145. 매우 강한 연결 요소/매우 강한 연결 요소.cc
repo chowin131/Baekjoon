185801c786a4bef2633c2efe7856df8e238f0827
chowin131 <chowin131@outlook.com>
2026-03-06 21:10:22 +0900

    [Platinum III] Title: 매우 강한 연결 요소, Time: 68 ms, Memory: 11424 KB -BaekjoonHub

--- 백준/Platinum/32145. 매우 강한 연결 요소/매우 강한 연결 요소.cc
+++ 백준/Platinum/32145. 매우 강한 연결 요소/매우 강한 연결 요소.cc
@@ -1,30 +1,27 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-#define int long long
+#define I long
 #define f first
 #define s second
-#define P pair<int,int>
-int ccw(P a,P b,P c){return(b.f-a.f)*(c.s-a.s)-(b.s-a.s)*(c.f-a.f);}
+#define P pair<I,I>
+I ccw(P a,P b,P c){return(b.f-a.f)*(c.s-a.s)-(b.s-a.s)*(c.f-a.f);}
 main(){
-    cin.tie(0)->sync_with_stdio(0);
-    int n,r;
-    cin>>n;
-    vector<P> pt(n),C;
-    for(auto&i:pt)cin>>i.f>>i.s;
-    sort(pt.begin(),pt.end(),[](P a,P b){
-        if(a.f==b.f)return a.s<b.s;
-        return a.f<b.f;
-    });
-    for(int i=0;i<n;i++){
-        while(C.size()>1&&ccw(C[C.size()-2],C.back(),pt[i])<0)C.pop_back();
-        C.push_back(pt[i]);
-    }
-    r=C.size();
-    C.clear();
-    for(int i=n-1;i>=0;i--){
-        while(C.size()>1&&ccw(C[C.size()-2],C.back(),pt[i])<0)C.pop_back();
-        C.push_back(pt[i]);
-    }
-    r+=C.size()-2;
-    cout<<3*n-r-3;
+cin.tie(0)->sync_with_stdio(0);
+I n,r,i;
+cin>>n;
+vector<P> p(n),C;
+for(auto&i:p)cin>>i.f>>i.s;
+sort(p.begin(),p.end());
+for(i=0;i<n;i++){
+while(C.size()>1&&ccw(C[C.size()-2],C.back(),p[i])<0)C.pop_back();
+C.push_back(p[i]);
+}
+r=C.size();
+C.clear();
+for(i=n-1;i>=0;i--){
+while(C.size()>1&&ccw(C[C.size()-2],C.back(),p[i])<0)C.pop_back();
+C.push_back(p[i]);
+}
+r+=C.size();
+cout<<3*n-r-1;
 }
