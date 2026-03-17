3ef2ff1a8a9696786409dcc14a4e12871a8de092
WOOIN CHOI <chowin131@outlook.com>
2026-03-17 17:54:55 +0900

    [Gold II] Title: 이차 방정식, Time: 80 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/34685. 이차 방정식/이차 방정식.cc
+++ 백준/Gold/34685. 이차 방정식/이차 방정식.cc
@@ -1,2 +1,2 @@
 #import<bits/stdc++.h>
-long k,S,i;main(){for(std::cin>>k;++i<=abs(k);S+=2*(k*k%i==0));std::cout<<S<<' '<<S*2*k;}
+long k,S,i;main(){std::cin>>k;for(;++i<=abs(k);)if(k*k%i==0)S+=2;std::cout<<S<<' '<<S*2*k;}
