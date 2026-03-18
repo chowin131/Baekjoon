7bb490f980d7d82711800ac55aab70790373e123
WOOIN CHOI <chowin131@outlook.com>
2026-03-18 11:04:22 +0900

    [Diamond V] Title: Binarytreefication, Time: 4 ms, Memory: 3236 KB, Score: 100 point -BaekjoonHub

--- 백준/Diamond/33808. Binarytreefication/Binarytreefication.cc
+++ 백준/Diamond/33808. Binarytreefication/Binarytreefication.cc
@@ -18,8 +18,7 @@ int main() {
     q.push(root);
     vector<vector<int>> new_T(22001);
     while (!q.empty()) {
-        int cur=q.front();
-        q.pop();
+        int cur=q.front(); q.pop();
         for (int next:T[cur]) {
             if (par[next]==0) {
                 q.push(next);
@@ -27,37 +26,42 @@ int main() {
             }
         }
     }
+    int maxch=0;
+    for (int v=1;v<=n;v++) {
+        int k=0;
+        for (int nb:T[v]) if (nb!=par[v]) k++;
+        maxch=max(maxch,k);
+    }
+    int H=0;
+    while ((1<<H)<maxch) H++;
+    if (H%2==1) H++;
     int val=n;
     q.push(root);
     while (!q.empty()) {
-        int tar=q.front();
-        q.pop();
+        int tar=q.front(); q.pop();
         vector<int> child;
         for (int nb:T[tar]) if (nb!=par[tar]) child.push_back(nb);
-        if (child.size()==0) continue;
-        else if (child.size()==1) {
-            int prev=tar;
-            for (int i=0;i<11;i++) {
-                new_T[prev].push_back(++val);
-                prev=val;
-            }
+        int k=child.size();
+        if (k==0) continue;
+
+        int h=0;
+        while ((1<<h)<k) h++;
+        int pad=H-h;
+
+        int prev=tar;
+        for (int i=0;i<pad;i++) {
+            new_T[prev].push_back(++val);
+            prev=val;
+        }
+
+        if (h==0) {
             new_T[prev].push_back(child[0]);
             q.push(child[0]);
         }
         else {
-            int k=child.size();
-            int h=0;
-            while ((1<<h)<k) h++;
-            int pad=11-h;
-            int prev=tar;
-            for (int i=0;i<pad;i++) {
-                new_T[prev].push_back(++val);
-                prev=val;
-            }
-            int R=prev;
             int sz=1<<(h+1);
             vector<int> nd(sz);
-            nd[1]=R;
+            nd[1]=prev;
             for (int i=2;i<sz;i++) nd[i]=++val;
             for (int i=1;i<(1<<h);i++) {
                 new_T[nd[i]].push_back(nd[2*i]);
@@ -71,9 +75,5 @@ int main() {
         }
     }
     cout<<val<<"\n";
-    for (int i=1;i<=val;i++) {
-        for (int j:new_T[i]) {
-            cout<<i<<" "<<j<<"\n";
-        }
-    }
+    for (int i=1;i<=val;i++) for (int j:new_T[i]) cout<<i<<" "<<j<<"\n";
 }
