399828ba68104360b844fe374e7a2cb52d5761fc
chowin <chowin131@outlook.com>
2026-03-25 15:11:21 +0900

    [Gold II] Title: 피보나치 수 3, Time: 12 ms, Memory: 17644 KB -BaekjoonHub

--- 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.cc
+++ 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.cc
@@ -1,13 +1,2 @@
-#include <iostream>
-using namespace std;
-long long arr[2000001];
-int main() {
-    arr[0] = 0;
-    arr[1] = 1;
-    long long N;
-    cin>>N;
-    N%=1500000;
-    for(int i=2;i<=N;i++)
-        arr[i]=(arr[i-1]+arr[i-2])%1000000;
-    cout<<arr[N];
-}
+#import<iostream>
+long a[2000001]={0,1},n,i=1;main(){std::cin>>n,n%=1500000;for(;i++<n;)a[i]=(a[i-1]+a[i-2])%1000000;std::cout<<a[n];}
