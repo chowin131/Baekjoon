04bca65ea5c64c091b633538fc097d5b867602c8
chowin <chowin131@outlook.com>
2026-03-29 18:01:18 +0900

    [Bronze V] Title: Конвейер, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/28938. Конвейер/Конвейер.cc
+++ 백준/Bronze/28938. Конвейер/Конвейер.cc
@@ -1,7 +1,2 @@
-#include<iostream>
-int s,x;
-main(){
-std::cin>>x;
-while(std::cin>>x)s+=x;
-std::cout<<(s<0?"Left":s?"Right":"Stay");
-}
+#import"bits/stdc++.h"
+int s,x;main(){std::cin>>x;while(std::cin>>x)s+=x;puts(s<0?"Left":s?"Right":"Stay");}
