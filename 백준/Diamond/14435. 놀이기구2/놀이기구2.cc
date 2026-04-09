6bca30524f5c53aba788630f38f13357ef607326
chowin <chowin131@outlook.com>
2026-04-09 22:13:47 +0900

    [Diamond I] Title: 놀이기구2, Time: 468 ms, Memory: 58224 KB -BaekjoonHub

--- 백준/Diamond/14435. 놀이기구2/놀이기구2.cc
+++ 백준/Diamond/14435. 놀이기구2/놀이기구2.cc
@@ -1,9 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define int long
-const int M=200005;
-int n,m,kk,q,lim[M],kid[M],qi[M],qj[M],qk[M],h[M],ti[M],tj[M],ans[M],cur,valid[M],i;
-priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>> pq[M];
+#define int long long
+
+const int MAXN=200005;
+int n,m,k_days,q;
+int lim[MAXN],kid[MAXN];
+int qi[MAXN],qj[MAXN],qk[MAXN];
+int h[MAXN],ti[MAXN],tj[MAXN];
+bool valid[MAXN];
+int ans[MAXN],cur=0;
+
+priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>> pq[MAXN];
+
 inline void add(int idx){
     int u=qi[idx],v=qj[idx],L=lim[qk[idx]];
     int half=(L-h[u]-h[v]+1)/2;
@@ -12,25 +20,30 @@ inline void add(int idx){
     pq[u].push({ti[idx],idx});
     pq[v].push({tj[idx],idx});
 }
-main(){
-    cin.tie(0)->sync_with_stdio(0);
-    cin>>n>>m>>kk>>q;
-    for(i=1;i<=m;i++) cin>>lim[i];
-    for(i=1;i<=kk;i++) cin>>kid[i];
-    for(i=1;i<=q;add(i++)) cin>>qi[i]>>qj[i]>>qk[i];
-    for(i=1;i<=kk;i++){
-        int g=((i>2&&ans[i-1]>ans[i-2])?2:1);
-        int x=kid[i];
+
+signed main(){
+    ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+    cin>>n>>m>>k_days>>q;
+    for(int i=1;i<=m;i++) cin>>lim[i];
+    for(int i=1;i<=k_days;i++) cin>>kid[i];
+    for(int i=1;i<=q;i++){
+        cin>>qi[i]>>qj[i]>>qk[i];
+        add(i);
+    }
+    for(int d=1;d<=k_days;d++){
+        int g=((d>2&&ans[d-1]>ans[d-2])?2:1);
+        int x=kid[d];
         h[x]+=g;
         while(!pq[x].empty()&&pq[x].top().first<=h[x]){
-            auto[t,idx]=pq[x].top();pq[x].pop();
+            auto[t,idx]=pq[x].top(); pq[x].pop();
             if(valid[idx]) continue;
             if(x==qi[idx]&&t!=ti[idx]) continue;
             if(x==qj[idx]&&t!=tj[idx]) continue;
-            if(h[qi[idx]]+h[qj[idx]]>=lim[qk[idx]]) valid[idx]=1,cur++;
+            int u=qi[idx],v=qj[idx];
+            if(h[u]+h[v]>=lim[qk[idx]]) valid[idx]=true,cur++;
             else add(idx);
         }
-        ans[i]=cur;
+        ans[d]=cur;
         cout<<cur<<"\n";
     }
 }
