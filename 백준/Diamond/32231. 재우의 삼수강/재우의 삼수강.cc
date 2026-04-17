4032e0428a6c0c86f3461da38f1dd20ed07a3bf0
chowin <chowin131@outlook.com>
2026-04-17 14:10:21 +0900

    [Diamond I] Title: 재우의 삼수강, Time: 12 ms, Memory: 2120 KB, Score: 100 point -BaekjoonHub

--- 백준/Diamond/32231. 재우의 삼수강/재우의 삼수강.cc
+++ 백준/Diamond/32231. 재우의 삼수강/재우의 삼수강.cc
@@ -2,10 +2,9 @@
 using namespace std;
 main(){
     cin.tie(0)->sync_with_stdio(0);
-    int t;
+    double t,x1,y1,x2,y2;
     cin>>t;
     while(t--){
-        double x1,y1,x2,y2;
         cin>>x1>>y1>>x2>>y2;
         double d=(x1-x2)*(x1-x2)+(y1-y2)*(y1-y2);
         cout<<fixed<<setprecision(6)<<acosh(1.0+d/(2.0*y1*y2))<<"\n";
