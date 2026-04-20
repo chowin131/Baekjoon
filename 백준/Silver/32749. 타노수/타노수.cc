c662d41c91e358f13a7aa63317c00df04392ecb9
chowin <chowin131@outlook.com>
2026-04-20 10:58:14 +0900

    [Silver V] Title: 타노수, Time: 8 ms, Memory: 5276 KB -BaekjoonHub

--- 백준/Silver/32749. 타노수/타노수.cc
+++ 백준/Silver/32749. 타노수/타노수.cc
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<string>
+#include<string_view>
 using namespace std;
 int main(){
 	cin.tie(0)->sync_with_stdio(0);
@@ -8,10 +9,10 @@ int main(){
 	string x;
 	cin>>x;
 	int s=1<<(n-t),b=1<<t;
-	string m="",v(x);
+	string_view m="",v(x);
 	for(int i=0;i<b;++i){
-		string c=v.substr(i*s,s);
+		string_view c=v.substr(i*s,s);
 		if(c>m)m=c;
 	}
-	cout<<m<<'\n';
+	cout<<m;
 }
