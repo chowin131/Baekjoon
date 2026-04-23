1c77365dbaced5ae6382de60522725465a327af7
chowin <chowin131@outlook.com>
2026-04-23 21:40:20 +0900

    [Bronze IV] Title: 운동장 한 바퀴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define double long double
+signed main(){
+cin.tie(0)->sync_with_stdio(0);
+int d1,d2;
+cin>>d1>>d2;
+cout<<fixed<<setprecision(6);
+cout<<d1*2+d2*2*3.141592;
+}
